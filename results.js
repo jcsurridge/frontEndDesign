1fcb2b9fbb47dbe52677df40f40ea80727ef866f
unknown <josh.c.surridge@gmail.com>
2019-11-26 17:34:11 -0700

    adding files

---
+++ results.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+    
+    $("#main_search").on("click", function () {
+      event.preventDefault();
+ 
+      let game = $("#game_input").val().trim();
+
+      console.log(game);
+
+      if (game) {
+      $("#intro").fadeOut("slow", function () {
+        //find both queries here with a function:
+            });
+        } 
+    });
+});
